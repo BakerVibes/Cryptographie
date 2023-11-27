4caf9c5017bf86e65029626b0893c790d1cbdd74
BakerVibes <114754952+BakerVibes@users.noreply.github.com>
2023-11-27 16:59:16 +0100

    Update affine.py

--- affine.py
+++ affine.py
@@ -8,22 +8,40 @@ def cryptage(chaine:str, a:int, b:int) -> str:
             lettre = chr(((a * (ord(i) - 65) + b) % 26) + 65)
         elif i.islower():
             lettre = chr(((a * (ord(i) - 97) + b) % 26) + 97)
-        elif i == ' ':
+        else:
             lettre = i
         message += lettre            
     return message
     
-#Entrer la valeur de a
+def decryptage(chaine: str, a: int, b: int) -> str:
+    # Faire le décryptage
+    a_inv = pow(a, -1, 26)  # Calcul de l'inverse modulaire de a modulo 26
+
+    message = ''
+    for i in chaine:
+        if i.isupper():
+            lettre = chr(((a_inv * (ord(i) - 65 - b)) % 26) + 65)
+        elif i.islower():
+            lettre = chr(((a_inv * (ord(i) - 97 - b)) % 26) + 97)
+        else:
+            lettre = i
+        message += lettre
+    return message
+
+choix = int(input("Tapez 1 pour crypter et 2 pour décrypter : "))
 a = int(input("Entrez la valeur du paramètre a (il doit être premier avec 26): "))
 
 # Vérifier si a et 26 sont premiers entre eux
 pgcd = math.gcd(a, 26)
-if pgcd == 1: # a et 26 sont premiers entre eux
-    #Entrer les valeurs de b et du texte à crypter
-    b = int(input("Entrez la valeur du paramètre b : "))
-    chaine = input("Entrez le texte que vous voulez crypter : ")
-    
-    #Afficher le message crypté
-    print("Message crypté : ",cryptage(chaine, a, b))
+if pgcd == 1: # a et 26 sont premiers entre eux    
+    b = int(input("Entrez la valeur du paramètre b : "))    
+    if choix == 1:
+        chaine = input("Entrez le texte que vous voulez crypter : ")
+        print("Message crypté : ",cryptage(chaine, a, b))
+    elif choix == 2:
+        chaine = input("Entrez le texte que vous voulez décrypter : ")
+        print("Message crypté : ",decryptage(chaine, a, b))
+    else:
+        print("Choix erroné!!!")
 else : # a et 26 ne sont pas premiers entre eux
-    print("Le nombre ",a, " n'est pas premier avec 26.")
+    print("Le nombre ",a, " n'est pas premier avec 26.")        
